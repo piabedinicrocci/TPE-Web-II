ea2a0327a8546fa756d8132b0dbd26dd435c4148
piabc <piabedinicrocci@hotmail.com>
2023-09-25 16:29:04 -0300

    Agregado de tuplas en el script de la bd

--- articulos.sql
+++ articulos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-09-2023 a las 05:40:30
+-- Tiempo de generación: 25-09-2023 a las 16:45:43
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -29,8 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categoria` (
   `id_categoria` int(11) NOT NULL,
-  `nombre` varchar(40) NOT NULL
+  `nombre` varchar(70) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id_categoria`, `nombre`) VALUES
+(1, 'anillos'),
+(2, 'aros'),
+(3, 'brazaletes'),
+(4, 'cadenas'),
+(5, 'gargantillas'),
+(6, 'pulseras'),
+(7, 'tobilleras');
 
 -- --------------------------------------------------------
 
@@ -47,6 +60,20 @@ CREATE TABLE `producto` (
   `stock` int(11) NOT NULL,
   `id_categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id_producto`, `nombre`, `descripcion`, `material`, `precio`, `stock`, `id_categoria`) VALUES
+(12874832, 'pulsera cielo', 'hecha de perlas extraidas de colombia', 'perlas', 1030.99, 11, 6),
+(12874833, 'gargantilla metálica regulable', NULL, 'perlas de metal', 4700, 28, 5),
+(12874834, 'cadena sahara', '25cm de largo con estilo bohemio', 'hilo', 2100, 28, 4),
+(12874835, 'anillo de plata', 'Diseño clásico de plata esterlina', 'plata', 4500, 10, 1),
+(12874836, 'aro de diamantes', 'Aros elegantes con diamantes incrustados', 'oro blanco', 25000, 5, 2),
+(12874837, 'pulsera de cuero', 'Pulsera de cuero genuino con cierre magnético', 'cuero', 1200, 15, 6),
+(12874838, 'gargantilla de perlas', 'Gargantilla con perlas cultivadas y broche de plata', 'perlas', 850, 8, 5),
+(12874839, 'brazalete mexico', NULL, 'alpaca', 2109, 2, 3);
 
 --
 -- Índices para tablas volcadas
@@ -73,23 +100,23 @@ ALTER TABLE `producto`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12874840;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `categoria`
+-- Filtros para la tabla `producto`
 --
-ALTER TABLE `categoria`
-  ADD CONSTRAINT `categoria_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `producto` (`id_categoria`) ON UPDATE CASCADE;
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
